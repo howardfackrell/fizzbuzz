b8193dfa99d062a9e5a619bb4dcd9c1f51389a65
Howard Fackrell <howard.fackrell@octanner.com>
2014-11-12 22:00:02 -0700

    prompt and add user input validation

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
-  runFizzBuzz(1, 100);
+	
+	var end = prompt("Enter an Integer:  ");
+
+	while (!isInteger(end)) {
+		end = prompt("Please enter a valid Integer");
+	}
+  	runFizzBuzzTo(+end);
 });
 
+function isInteger(n) {
+	var re=/^-?[0-9]+$/;
+	return re.test(n);
+}
+
+function runFizzBuzzTo(end) {
+	runFizzBuzz(1, end);
+}
 function runFizzBuzz(start, end) {
 	for(var i = start; i <= end; i++) {
 		var fb = fizzbuzz(i);
